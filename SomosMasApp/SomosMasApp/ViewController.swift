5c7a04ef9e893c0d803dd537006dae04ae772ed9
Francisco <80861248+frannasich@users.noreply.github.com>
2022-03-16 14:49:46 -0300

    Project created (#5)
    * Creación del proyecto
    
    * SignIn and SignUp folders added
    
    * SignIn and SignUp folders added
    
    * Some folders deleted
    
    * gitignore added
    
    * Delete SomosMasApp/SomosMasApp.xcodeproj/xcuserdata/fran.xcuserdatad/xcschemes directory

---
+++ SomosMasApp/SomosMasApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SomosMasApp
+//
+//  Created by Fran Nasich on 16/03/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
