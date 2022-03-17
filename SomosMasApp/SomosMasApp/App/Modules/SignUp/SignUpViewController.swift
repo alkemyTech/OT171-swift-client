bb162b0858d1f50e5926b0d7e4a8ca1a3b410e3e
Francisco Nasich <frannasich@gmail.com>
2022-03-16 21:58:50 -0300

    View labels conected with Controller

--- SomosMasApp/SomosMasApp/App/Modules/SignUp/SignUpViewController.swift
+++ SomosMasApp/SomosMasApp/App/Modules/SignUp/SignUpViewController.swift
@@ -9,21 +9,13 @@ import UIKit
 
 class SignUpViewController: UIViewController {
 
+    @IBOutlet weak var signUpMailLabel: UITextField!
+    @IBOutlet weak var signUpPassLabel: UITextField!
+    @IBOutlet weak var signUpConfirmPassLabel: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
