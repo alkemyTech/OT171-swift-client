576bbf7078e94fc725ded5be9c58c5ab00a8d86f
Francisco Nasich <frannasich@gmail.com>
2022-03-18 10:42:21 -0300

    Username label and text field added to the SignUpView.

--- SomosMasApp/SomosMasApp/Modules/SignUp/Controller/SignUpViewController.swift
+++ SomosMasApp/SomosMasApp/Modules/SignUp/Controller/SignUpViewController.swift
@@ -9,12 +9,13 @@ import UIKit
 
 class SignUpViewController: UIViewController {
 
+    @IBOutlet weak var signUpUsernameLabel: UITextField!
     @IBOutlet weak var signUpMailLabel: UITextField!
     @IBOutlet weak var signUpPassLabel: UITextField!
     @IBOutlet weak var signUpConfirmPassLabel: UITextField!
     
     override func viewDidLoad() {
-        super.viewDidLoad()
+        super.viewDidLoad()        
 
     }
 
