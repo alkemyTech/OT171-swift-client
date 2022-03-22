18e03f5d439bd6108044dff30dbdfb13f1a811e1
Mariano Battaglia <battaglia1902@gmail.com>
2022-03-22 00:30:46 -0300

    Correction of problems with Git and incorporation of username field to the Sign Up validation

--- SomosMasApp/SomosMasApp/App/Extensions/Extensions.swift
+++ SomosMasApp/SomosMasApp/App/Extensions/Extensions.swift
@@ -10,6 +10,10 @@ import Foundation
 extension String {
     
     // Regular expressions to validate fields
+    var isValidUser: Bool {
+        NSPredicate(format: "SELF MATCHES %@", "^(?=.{6,12}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$").evaluate(with: self)
+    }
+    
     var isValidEmail: Bool {
         NSPredicate(format: "SELF MATCHES %@", "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}").evaluate(with: self)
     }
@@ -19,4 +23,3 @@ extension String {
     }
     
 }
-
