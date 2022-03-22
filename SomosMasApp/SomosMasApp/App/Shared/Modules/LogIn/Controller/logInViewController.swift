026bb961da4e382122b215168f74dab972ee91a3
Francisco Nasich <frannasich@gmail.com>
2022-03-22 15:01:51 -0300

    Button of navigation to sign up from log in added.

--- SomosMasApp/SomosMasApp/App/Shared/Modules/LogIn/Controller/logInViewController.swift
+++ SomosMasApp/SomosMasApp/App/Shared/Modules/LogIn/Controller/logInViewController.swift
@@ -16,22 +16,15 @@ class logInViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
 
     @IBAction func logInButtonPressed(_ sender: UIButton) {
         
     }
     
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func signUpButtonPressed(_ sender: UIButton) {
+        let signUpController = SignUpViewController()
+        self.navigationController?.pushViewController(signUpController, animated: true)
     }
-    */
-
+    
 }
