3ba2b22a0efc0d384ad90e81491a19a4784ca653
Francisco <80861248+frannasich@users.noreply.github.com>
2022-03-22 22:44:20 -0300

    [OT171-20] Button navigation to SignUp from LogInView (#15)
    * added the view of the log in screen (logInView) also added the view controller for that view (logInViewController)
    
    * updated the log in layaout
    
    * updated constraints
    
    * changed the initial VC
    
    * removed the xuserdata file
    
    * removed the file SomosMasApp/SomosMasApp.xcodeproj/project.xcworkspace/xcuserdata/maximilianoituarte.xcuserdatad/UserInterfaceState.xcuserstate
    
    * Added folders that I hadn't before.
    
    * Button of navigation to sign up from log in added.
    
    Co-authored-by: Maximiliano Ituarte <maximiliano.ituarte@gmail.com>

--- SomosMasApp/SomosMasApp/SceneDelegate.swift
+++ SomosMasApp/SomosMasApp/SceneDelegate.swift
@@ -17,12 +17,13 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let windowScene1 = (scene as? UIWindowScene) else { return }
         
-        let window = UIWindow(windowScene: windowScene1)
-        window.rootViewController = logInViewController()
-        window.makeKeyAndVisible()
-        self.window = window
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow (windowScene: windowScene)
+        let mainController = logInViewController()
+        let navigationController = UINavigationController(rootViewController: mainController)
+        window?.rootViewController = navigationController
+        window!.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
