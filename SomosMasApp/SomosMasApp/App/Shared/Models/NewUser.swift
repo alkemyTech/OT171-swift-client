8927cb679b90b8e2e64299f503843f214258d7a2
BraianTheiler <braian_theiler@hotmail.com>
2022-03-23 13:44:07 -0300

    Api integration and incorporation a new user

---
+++ SomosMasApp/SomosMasApp/App/Shared/Models/NewUser.swift
@@ -0,0 +1,14 @@
+//
+//  NewUser.swift
+//  SomosMasApp
+//
+//  Created by Braian Theiler on 23/03/2022.
+//
+
+import Foundation
+
+struct NewUser: Codable {
+    let name: String
+    let email: String
+    let password: String
+}
