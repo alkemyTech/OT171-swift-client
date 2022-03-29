b5014d4233ee4fec373aef72f52e2a3a16a8d53c
Hilario Cuervo <hilariocuervo2@gmail.com>
2022-03-28 22:46:49 -0300

    Previous mistake deleted and setting the rootVC to SplashVC

--- SomosMasApp/SomosMasApp/AppDelegate.swift
+++ SomosMasApp/SomosMasApp/AppDelegate.swift
@@ -11,11 +11,10 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        
-        sleep(5)
-        
+                
         return true
     }
+    
 
     // MARK: UISceneSession Lifecycle
 
