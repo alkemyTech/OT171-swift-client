1102460eb0f3906d6dcf0c48afb56531428b59f7
BraianTheiler <braian_theiler@hotmail.com>
2022-03-30 09:29:29 -0300

    Finished image slider in home

---
+++ SomosMasApp/SomosMasApp/App/Shared/Modules/Home/Controller/HomeCollectionViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  HomeCollectionViewCell.swift
+//  SomosMasApp
+//
+//  Created by Braian Theiler on 29/03/2022.
+//
+
+import UIKit
+
+class HomeCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var myImage: UIImageView!
+    @IBOutlet weak var myTitle: UILabel!
+    @IBOutlet weak var myDescription: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        myTitle.font = UIFont.boldSystemFont(ofSize: 20)
+    }
+
+}
