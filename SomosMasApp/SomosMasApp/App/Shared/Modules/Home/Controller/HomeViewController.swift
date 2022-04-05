01cc2e73a4dfd6bca2ea0dd3f37d4b00d7c84016
Braian Theiler <74666277+braiantheiler@users.noreply.github.com>
2022-04-05 10:18:47 -0300

    [OT171-27] Redirect the app from Log In to Home (#27)
    * fix .gitignore
    
    * Modules added
    
    * Folders updapted
    
    * Fix Modules
    
    * Api integration and incorporation a new user
    
    * redirect signUp to logIn
    
    * Images added in the assets
    
    * File SliderData with information about slider
    
    * Finished image slider in home
    
    * Fix file
    
    * file binary deleted
    
    * Button to close app added in navigation bar (Section Home)
    
    Co-authored-by: BraianTheiler <braian_theiler@hotmail.com>

--- SomosMasApp/SomosMasApp/App/Shared/Modules/Home/Controller/HomeViewController.swift
+++ SomosMasApp/SomosMasApp/App/Shared/Modules/Home/Controller/HomeViewController.swift
@@ -33,6 +33,12 @@ class HomeViewController: UIViewController {
         collectionView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
         collectionView.register(UINib(nibName: "HomeCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "mycell")
         
+        let backButton = UIBarButtonItem(title: "Close", style: .plain, target: self, action: #selector(closeApp))
+        self.navigationItem.leftBarButtonItem  = backButton
+    }
+    @objc func closeApp() {
+        exit(0)
+
     }
     
 }
