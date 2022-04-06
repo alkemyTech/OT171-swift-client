47e782327ff48321d8844cb370477bd3dd8e30f6
Maximiliano Ituarte <maximiliano.ituarte@gmail.com>
2022-04-06 10:52:21 -0300

    final changes with last version of the main

---
+++ SomosMasApp/SomosMasApp/App/Extensions/ImageViewExtension.swift
@@ -0,0 +1,22 @@
+//
+//  ImageViewExtension.swift
+//  SomosMasApp
+//
+//  Created by Maximiliano Ituarte on 06/04/2022.
+//
+
+import UIKit
+
+extension UIImageView {
+    func load(url: URL) {
+        DispatchQueue.global().async { [weak self] in
+            if let data = try? Data(contentsOf: url) {
+                if let image = UIImage(data: data) {
+                    DispatchQueue.main.async {
+                        self?.image = image
+                    }
+                }
+            }
+        }
+    }
+}
