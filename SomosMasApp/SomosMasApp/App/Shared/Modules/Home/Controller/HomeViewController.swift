42bf5d90994cd556e54bf266a7f4564687253f1b
Mariano Battaglia <battaglia1902@gmail.com>
2022-04-05 23:55:49 -0300

    Pull main and correct conflicts to push files

--- SomosMasApp/SomosMasApp/App/Shared/Modules/Home/Controller/HomeViewController.swift
+++ SomosMasApp/SomosMasApp/App/Shared/Modules/Home/Controller/HomeViewController.swift
@@ -14,12 +14,7 @@ class HomeViewController: UIViewController {
         let description: String?
         let image: UIImage?
     }
-    
-    struct TestimonialsData {
-        let image: UIImage?
-        let epigraph: String?
-    }
-    
+        
     struct LastestNewsData {
         let image: UIImage?
         let epigraph: String?
@@ -31,11 +26,7 @@ class HomeViewController: UIViewController {
                        SliderData(title: "Victoria Sanchez", description: "Tesorera", image: UIImage(named:"Image_4")),
                        SliderData(title: "Martina Diglido", description: "Marketing", image: UIImage(named:"Image_5"))
                      ]
-    
-    var testimonialsData = [ TestimonialsData(image: UIImage(named:"Image_6"), epigraph: "Epígrafe requerido para esta imagen"),
-                            TestimonialsData(image: UIImage(named:"Image_7"), epigraph: "Epígrafe requerido para esta imagen"),
-                          ]
-    
+        
     let lastestNewsData = [ LastestNewsData(image: UIImage(named:"Image_1"), epigraph: "Epígrafe para imagen 1"),
                             LastestNewsData(image: UIImage(named:"Image_2"), epigraph: "Epígrafe para imagen 2"),
                             LastestNewsData(image: UIImage(named:"Image_3"), epigraph: "Epígrafe para imagen 3"),
@@ -43,7 +34,6 @@ class HomeViewController: UIViewController {
                           ]
     
     @IBOutlet weak var collectionView: UICollectionView!
-    @IBOutlet weak var testimonialsCollectionView: UICollectionView!
     @IBOutlet weak var lastestNewsCollectionView: UICollectionView!
     
     override func viewDidLoad() {
@@ -54,12 +44,6 @@ class HomeViewController: UIViewController {
         collectionView.delegate = self
         collectionView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
         collectionView.register(UINib(nibName: "HomeCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "mycell")
-        testimonialsCollectionView.isPagingEnabled = true
-        testimonialsCollectionView.dataSource = self
-        testimonialsCollectionView.delegate = self
-        testimonialsCollectionView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
-        testimonialsCollectionView.register(UINib(nibName: "TestimonialsCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "Tcell")
-        testimonialsCollectionView.register(UINib(nibName: "SeeMoreCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "seeMoreCell")
         lastestNewsCollectionView.isPagingEnabled = true
         lastestNewsCollectionView.dataSource = self
         lastestNewsCollectionView.delegate = self
@@ -84,11 +68,8 @@ extension HomeViewController: UICollectionViewDataSource, UICollectionViewDelega
 
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         switch collectionView {
-        case self.testimonialsCollectionView:
-            // Return Max pages = 4 and add 1 more for item "Ver más"
-            return min(testimonialsData.count + 1, 5)
         case self.lastestNewsCollectionView:
-            // Return Max pages = 4 and add 1 more for item "Ver más". Its another case becouse takes data from another Array
+            // Return Max pages = 4 and add 1 more for item "Ver más".
             return min(lastestNewsData.count + 1, 5)
         default:
             return sliderData.count
@@ -97,20 +78,6 @@ extension HomeViewController: UICollectionViewDataSource, UICollectionViewDelega
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         switch collectionView{
-        case testimonialsCollectionView:
-            // Add seeMore page
-            if indexPath.row == min(testimonialsData.count, 4) {
-                let cell = testimonialsCollectionView.dequeueReusableCell(withReuseIdentifier: "seeMoreCell", for: indexPath) as? SeeMoreCollectionViewCell
-                
-                return cell ?? SeeMoreCollectionViewCell()
-            } else {
-            let cell = testimonialsCollectionView.dequeueReusableCell(withReuseIdentifier: "Tcell", for: indexPath) as? TestimonialsCollectionViewCell
-            
-            cell?.testimonialImage.image = testimonialsData[indexPath.row].image
-            cell?.testimonialEpigraph.text = testimonialsData[indexPath.row].epigraph
-            
-            return cell ?? TestimonialsCollectionViewCell()
-            }
         case lastestNewsCollectionView:
             if indexPath.row == min(lastestNewsData.count, 4) {
                 let cell = lastestNewsCollectionView.dequeueReusableCell(withReuseIdentifier: "seeMoreCell", for: indexPath) as? SeeMoreCollectionViewCell
@@ -138,10 +105,6 @@ extension HomeViewController: UICollectionViewDataSource, UICollectionViewDelega
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath){
         collectionView.deselectItem(at: indexPath, animated: true)
         switch collectionView {
-        case testimonialsCollectionView:
-            if indexPath.row == min(testimonialsData.count, 4) {
-                // Add an action when the item is selected
-            }
         case lastestNewsCollectionView:
             if indexPath.row == min(lastestNewsData.count, 4) {
                 // Add an action when "ver Más" item from "Ultimas novedades" is selected
---
+++ SomosMasApp/SomosMasApp/App/Shared/Modules/Home/Controller/seeMoreCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  SeeMoreCollectionViewCell.swift
+//  SomosMasApp
+//
+//  Created by Fran Nasich on 05/04/2022.
+//
+
+import UIKit
+
+class SeeMoreCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var seeMoreLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
