691f40f5aa544a24bd03f6ffe2f6650466ec515d
Mariano Battaglia <battaglia1902@gmail.com>
2022-04-07 19:50:39 -0300

    NewService added

--- SomosMasApp/SomosMasApp/App/Shared/Modules/Home/Services/NewsService.swift
+++ SomosMasApp/SomosMasApp/App/Shared/Modules/Home/Services/NewsService.swift
@@ -10,5 +10,26 @@ import Foundation
 class NewsService {
     let webserviceURL = Bundle.main.object(forInfoDictionaryKey: "WEBSERVICE_URL") as! String
     
-    func getLastestNewsData(completion: @escaping)
+    func getLastestNewsData(onComplete: @escaping ([NewsList]) -> Void, onError: @escaping () -> Void) {
+        ApiManager.shared.get(url: "\(webserviceURL)api/news") { response in
+            switch response {
+            case .success(let data):
+                do{
+                    if let data = data {
+                        let decoder = JSONDecoder()
+                        decoder.keyDecodingStrategy = .convertFromSnakeCase
+                        let newsResponse = try decoder.decode(NewsListResponse.self, from: data)
+                        onComplete(newsResponse.data)
+                    } else {
+                        onError()
+                    }
+                } catch {
+                    onError()
+                    print(error)
+                }
+            case.failure(_):
+                onError()
+            }
+        }
+    }
 }
