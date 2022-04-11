5241a9bef5a79f86b19e3c8a5ca8438ef9579563
Mariano Battaglia <battaglia1902@gmail.com>
2022-04-11 19:29:05 -0300

    Resolved conflicts

--- SomosMasApp/SomosMasApp/App/Shared/Modules/Home/Controller/HomeViewController.swift
+++ SomosMasApp/SomosMasApp/App/Shared/Modules/Home/Controller/HomeViewController.swift
@@ -11,36 +11,30 @@ protocol SliderListDelegate {
     func hideTestimonials()
     func reloadTestimonials()
     func reloadSlider()
+    func reloadNews()
+    func hideSectionsWithoutData()
 }
 
 class HomeViewController: UIViewController {
-    
-    struct LastestNewsData {
-        let image: UIImage?
-        let epigraph: String?
-    }
-    
-    let lastestNewsData = [ LastestNewsData(image: UIImage(named:"Image_1"), epigraph: "Epígrafe para imagen 1"),
-                            LastestNewsData(image: UIImage(named:"Image_2"), epigraph: "Epígrafe para imagen 2"),
-                            LastestNewsData(image: UIImage(named:"Image_3"), epigraph: "Epígrafe para imagen 3"),
-                            LastestNewsData(image: UIImage(named:"Image_4"), epigraph: "Epígrafe para imagen 4")
-                          ]
     
     @IBOutlet weak var testimonialsTitleLabel: UILabel!
     @IBOutlet weak var collectionView: UICollectionView!
     @IBOutlet weak var testimonialsCollectionView: UICollectionView!
     @IBOutlet weak var lastestNewsCollectionView: UICollectionView!
+    @IBOutlet weak var lastestNewsTitleLabel: UILabel!
     
     private let serviceSlider = SliderService()
     private let serviceTestimonials = TestimonialsService()
+    private let serviceNews = NewsService()
     private var sliderViewModel: SliderViewModel?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.sliderViewModel = SliderViewModel(service1: serviceSlider, service2: serviceTestimonials, delegate: self)
+        self.sliderViewModel = SliderViewModel(service1: serviceSlider, service2: serviceTestimonials, service3: serviceNews, delegate: self)
         self.sliderViewModel?.getSliders()
         self.sliderViewModel?.getTestimonials()
+        self.sliderViewModel?.getNews()
         collectionView.isPagingEnabled = true
         collectionView.dataSource = self
         collectionView.delegate = self
@@ -57,7 +51,13 @@ class HomeViewController: UIViewController {
         lastestNewsCollectionView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
         lastestNewsCollectionView.register(UINib(nibName: "NewsCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "newscell")
         lastestNewsCollectionView.register(UINib(nibName: "SeeMoreCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "seeMoreCell")
-
+        lastestNewsCollectionView.isPagingEnabled = true
+        lastestNewsCollectionView.dataSource = self
+        lastestNewsCollectionView.delegate = self
+        lastestNewsCollectionView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        lastestNewsCollectionView.register(UINib(nibName: "NewsCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "newscell")
+        lastestNewsCollectionView.register(UINib(nibName: "SeeMoreCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "seeMoreCell")
+        
         let backButton = UIBarButtonItem(title: "Close", style: .plain, target: self, action: #selector(closeApp))
         self.navigationItem.leftBarButtonItem  = backButton
     }
@@ -80,7 +80,7 @@ extension HomeViewController: UICollectionViewDataSource, UICollectionViewDelega
             return min(sliderViewModel!.getTestimonialsCount() + 1, 5)
         case self.lastestNewsCollectionView:
             // Return Max pages = 4 and add 1 more for item "Ver más". Its another case becouse takes data from another Array
-            return min(lastestNewsData.count + 1, 5)
+            return min(sliderViewModel!.getNewsCount() + 1, 5)
         default:
             return self.sliderViewModel?.getSlidersCount() ?? 0
         }
@@ -107,16 +107,20 @@ extension HomeViewController: UICollectionViewDataSource, UICollectionViewDelega
             return cell ?? TestimonialsCollectionViewCell()
             }
         case lastestNewsCollectionView:
-            if indexPath.row == min(lastestNewsData.count, 4) {
+            if indexPath.row == min(sliderViewModel!.getNewsCount(), 4) {
                 let cell = lastestNewsCollectionView.dequeueReusableCell(withReuseIdentifier: "seeMoreCell", for: indexPath) as? SeeMoreCollectionViewCell
 
                 return cell ?? SeeMoreCollectionViewCell()
             } else {
             let cell = lastestNewsCollectionView.dequeueReusableCell(withReuseIdentifier: "newscell", for: indexPath) as? NewsCollectionViewCell
             
-            cell?.newsImage.image = lastestNewsData[indexPath.row].image
-            cell?.newsDescription.text = lastestNewsData[indexPath.row].epigraph
-            
+                let imagePath = self.sliderViewModel?.getNews(at: indexPath.row).image
+                let imageUrl = URL(string: imagePath!)
+                
+                cell?.newsImage.contentMode = .scaleAspectFit
+                cell?.newsImage.load(url: imageUrl!)
+                cell?.newsDescription.text = sliderViewModel?.getNews(at: indexPath.row).name
+                
             return cell ?? NewsCollectionViewCell()
             }
         default:
@@ -143,7 +147,7 @@ extension HomeViewController: UICollectionViewDataSource, UICollectionViewDelega
                 // Add an action when the item is selected
             }
         case lastestNewsCollectionView:
-            if indexPath.row == min(lastestNewsData.count, 4) {
+            if indexPath.row == min(sliderViewModel!.getNewsCount(), 4) {
                 // Add an action when "ver Más" item from "Ultimas novedades" is selected
             }
         default:
@@ -166,10 +170,24 @@ extension HomeViewController: UICollectionViewDelegateFlowLayout {
 }
 
 extension HomeViewController: SliderListDelegate{
+    func hideSectionsWithoutData() {
+        if self.sliderViewModel?.getNewsCount() == 0 {
+            // Hide section if service doesn't return data
+            self.lastestNewsCollectionView.isHidden = true
+            self.lastestNewsTitleLabel.isHidden = true
+            // Reduce height constraint to zero
+            self.lastestNewsCollectionView.heightAnchor.constraint(equalToConstant: 0).isActive = true
+            self.lastestNewsTitleLabel.heightAnchor.constraint(equalToConstant: 0).isActive = true
+        }
+    }
     
     func hideTestimonials() {
         self.testimonialsCollectionView.isHidden = true
         self.testimonialsTitleLabel.isHidden = true
+    }
+
+    func reloadNews() {
+        self.lastestNewsCollectionView.reloadData()
     }
 
     func reloadTestimonials() {
--- SomosMasApp/SomosMasApp/App/Shared/Modules/Home/ViewModel/HomeViewModel.swift
+++ SomosMasApp/SomosMasApp/App/Shared/Modules/Home/ViewModel/HomeViewModel.swift
@@ -13,11 +13,14 @@ class SliderViewModel {
     private var slidersResponded = [SlidersList]()
     private var testimonialsService: TestimonialsService
     private var testimonialsResponse = [TestimonialsList]()
+    private var newsService: NewsService
+    private var newsResponse = [NewsList]()
     private var delegate: SliderListDelegate
     
-    init(service1: SliderService, service2: TestimonialsService, delegate: SliderListDelegate) {
+    init(service1: SliderService, service2: TestimonialsService, service3: NewsService, delegate: SliderListDelegate) {
         self.sliderService = service1
         self.testimonialsService = service2
+        self.newsService = service3
         self.delegate = delegate
     }
     
@@ -40,6 +43,16 @@ class SliderViewModel {
         }
     }
     
+    func getNews() {
+        newsService.getLastestNewsData {response in
+            self.newsResponse = response
+            self.delegate.reloadNews()
+        } onError: {
+            self.delegate.reloadNews()
+            self.delegate.hideSectionsWithoutData()
+        }
+    }
+    
     func getSliders(at index: Int) -> SlidersList {
         return slidersResponded[index]
     }
@@ -48,11 +61,19 @@ class SliderViewModel {
         return testimonialsResponse[index]
     }
     
+    func getNews(at index: Int) -> NewsList {
+        return newsResponse[index]
+    }
+    
     func getSlidersCount() -> Int {
         return slidersResponded.count
     }
     
     func getTestimonialsCount() -> Int {
         return testimonialsResponse.count
+    }
+    
+    func getNewsCount() -> Int {
+        return newsResponse.count
     }
 }
