f734bc360596ba11209f5daadab21a23c6518094
Francisco <80861248+frannasich@users.noreply.github.com>
2022-04-11 15:10:50 -0300

    [OT171-40]  Testimonials integration (#31)
    * TestimonialsList, TestimonialsResponse and TestimonialsService added
    
    * override deleted
    
    * TestimonialsViewModel added
    
    * cellForItemAt and didSelected Testimonials updated
    
    * Fixed some errors in Splash
    
    * Changed Description for name in TeestimonialsList
    
    * Add condition to Hide Testimonials section
    
    * Updated HomeViewModel and HomeViewController
    
    * Identifier updated
    
    * Added description to Testimonials
    
    * Some wrong logics deleted
    
    * Functions to hide section and label added
    
    * Function hideTestimonialWithoutData added
    
    * Change the way to connect vm with vm
    
    * Merge branch 'main' into OT171-40_Integracion_Testimonios_prueba
    
    * main:
      Ot171 - 44 - Navigation to "nosotros" View (#32)
    
    # Conflicts:
    #	SomosMasApp/SomosMasApp.xcodeproj/project.pbxproj
    #	SomosMasApp/SomosMasApp/App/Shared/Modules/Splash/Controller/SplashViewController.swift
    
    * no message
    
    * Update func hideTestimonials and added ternary operator

--- SomosMasApp/SomosMasApp/App/Shared/Modules/Home/ViewModel/HomeViewModel.swift
+++ SomosMasApp/SomosMasApp/App/Shared/Modules/Home/ViewModel/HomeViewModel.swift
@@ -11,21 +11,32 @@ class SliderViewModel {
     
     private var sliderService: SliderService
     private var slidersResponded = [SlidersList]()
+    private var testimonialsService: TestimonialsService
+    private var testimonialsResponse = [TestimonialsList]()
     private var delegate: SliderListDelegate
     
-    init(service: SliderService, delegate: SliderListDelegate) {
-        self.sliderService = service
+    init(service1: SliderService, service2: TestimonialsService, delegate: SliderListDelegate) {
+        self.sliderService = service1
+        self.testimonialsService = service2
         self.delegate = delegate
-        
     }
     
     func getSliders(){
         sliderService.getAllSliders {response in
             self.slidersResponded = response
-       
             self.delegate.reloadSlider()
         } onError: {
             self.delegate.reloadSlider()
+        }
+    }
+    
+    func getTestimonials() {
+        testimonialsService.getAllSliders {response in
+        self.testimonialsResponse = response
+            self.getTestimonialsCount() == 0 ? self.delegate.hideTestimonials()
+            : self.delegate.reloadTestimonials()
+        } onError: {
+            self.delegate.hideTestimonials()
         }
     }
     
@@ -33,10 +44,15 @@ class SliderViewModel {
         return slidersResponded[index]
     }
     
+    func getTestimonial(at index: Int) -> TestimonialsList {
+        return testimonialsResponse[index]
+    }
+    
     func getSlidersCount() -> Int {
         return slidersResponded.count
     }
+    
+    func getTestimonialsCount() -> Int {
+        return testimonialsResponse.count
+    }
 }
-
-
-
