5a834310f6249aa5a0da972b1537d9a66eca15d5
Maximiliano Ituarte <36338503+mituartel@users.noreply.github.com>
2022-04-12 12:52:10 -0300

    Ot171 - 45 - Nosotros Design (#37)
    * made changes with the last version of main
    
    * change the position of the text view and the collection view
    
    * completed
    
    * minor changes on the font of the label
    
    * deleted a line that i dont use

--- SomosMasApp/SomosMasApp/App/Shared/Modules/Nosotros/ViewController/NosotrosViewController.swift
+++ SomosMasApp/SomosMasApp/App/Shared/Modules/Nosotros/ViewController/NosotrosViewController.swift
@@ -7,15 +7,72 @@
 
 import UIKit
 
-class NosotrosViewController: UIViewController {
 
+struct NosotrosData {
+    let image: UIImage?
+    let name: String?
+    let jobTitle: String?
+}
+
+
+class NosotrosViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource,UICollectionViewDelegateFlowLayout {
+    
+
+    let nosotrosData = [ NosotrosData(image: UIImage(named:"Image_5"), name: "Cecilia Mendez", jobTitle:    "Puesto 1"),
+                         NosotrosData(image: UIImage(named:"Image_1"), name: "Juan Perez", jobTitle: "Puesto 2"),
+                         NosotrosData(image: UIImage(named:"Image_4"), name: "Maria Garcia", jobTitle: "Puesto 3"),
+                         NosotrosData(image: UIImage(named:"Image_5"), name: "Maria Irola", jobTitle: "Puesto 4"),
+                         NosotrosData(image: UIImage(named:"Image_4"), name: "Marita Gomez", jobTitle: "Puesto 5"),
+                         NosotrosData(image: UIImage(named:"Image_2"), name: "Miriam Diaz", jobTitle: "Puesto 6"),
+                         NosotrosData(image: UIImage(named:"Image_3"), name: "Rodrigo Fuente", jobTitle: "Puesto 7"),
+                         NosotrosData(image: UIImage(named:"Image_1"), name: "Juan Perez", jobTitle: "Puesto 2"),
+                         NosotrosData(image: UIImage(named:"Image_4"), name: "Maria Garcia", jobTitle: "Puesto 3"),
+                         NosotrosData(image: UIImage(named:"Image_5"), name: "Maria Irola", jobTitle: "Puesto 4"),
+                         NosotrosData(image: UIImage(named:"Image_4"), name: "Marita Gomez", jobTitle: "Puesto 5"),
+                         NosotrosData(image: UIImage(named:"Image_2"), name: "Miriam Diaz", jobTitle: "Puesto 6"),
+                         NosotrosData(image: UIImage(named:"Image_3"), name: "Rodrigo Fuente", jobTitle: "Puesto 7")
+                          ]
+    
+    @IBOutlet var nosotrosCollectionView: UICollectionView!
+    
+    
+    @IBOutlet var label: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        self.nosotrosCollectionView.delegate = self
+        self.nosotrosCollectionView.dataSource = self
+        
+        label.adjustsFontSizeToFitWidth = true
+       
+        nosotrosCollectionView.register(UINib(nibName: "NosotrosCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "nosotrosCell")
+    
     }
 
-
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return nosotrosData.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "nosotrosCell", for: indexPath) as? NosotrosCollectionViewCell
+        
+        cell?.name.text = nosotrosData[indexPath.row].name
+        cell?.jobTitle.text = nosotrosData[indexPath.row].jobTitle
+        cell?.photo.image = nosotrosData[indexPath.row].image
+        
+        
+        return cell ?? NosotrosCollectionViewCell()
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize
+    {
+        let columnCount = 3
+        let width  = (Int(view.frame.width) - 20) / columnCount
+        return CGSize(width: width, height: width)
+    }
+    
+    
     /*
     // MARK: - Navigation
 
