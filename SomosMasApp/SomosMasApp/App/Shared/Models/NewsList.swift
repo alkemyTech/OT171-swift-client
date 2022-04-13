b8e6b43ff0fa43e74c235b2059014f43be594259
Mariano Battaglia <94753551+marianobattaglia@users.noreply.github.com>
2022-04-13 10:36:55 -0300

    [171-37] Lastest News API Integration (#35)
    * NewsList and NewsListResponse added
    
    * get service created on NewsService
    
    * integrate service to HomeViewController and deleted override from NewsCollectionViewCell
    
    * hide section in process
    
    * deleted last ticket struct and array in HomeViewController
    
    * fit content of cell in view controller
    
    * Reduce constraint to zero if API doesnt return data
    
    * Resolved conflicts
    
    * correct force unwrap
    
    * move logical from VC to VM (hideSectionsWithoutData function)

---
+++ SomosMasApp/SomosMasApp/App/Shared/Models/NewsList.swift
@@ -0,0 +1,13 @@
+//
+//  NewsList.swift
+//  SomosMasApp
+//
+//  Created by Mariano Battaglia on 08/04/2022.
+//
+
+import Foundation
+
+struct NewsList : Codable {
+    let image: String?
+    let name: String?
+}
---
+++ SomosMasApp/SomosMasApp/App/Shared/Modules/Home/Services/NewsService.swift
@@ -0,0 +1,34 @@
+//
+//  NewsService.swift
+//  SomosMasApp
+//
+//  Created by Mariano Battaglia on 08/04/2022.
+//
+
+import Foundation
+
+class NewsService {
+    let webserviceURL = Bundle.main.object(forInfoDictionaryKey: "WEBSERVICE_URL") as! String
+    
+    func getLastestNewsData(onComplete: @escaping ([NewsList]) -> Void, onError: @escaping () -> Void) {
+        ApiManager.shared.get(url: "\(webserviceURL)api/news") { response in
+            switch response {
+            case .success(let data):
+                do{
+                    if let data = data {
+                        let decoder = JSONDecoder()
+                        decoder.keyDecodingStrategy = .convertFromSnakeCase
+                        let newsResponse = try decoder.decode(NewsListResponse.self, from: data)
+                        onComplete(newsResponse.data)
+                    } else {
+                        onError()
+                    }
+                } catch {
+                    onError()
+                }
+            case.failure(_):
+                onError()
+            }
+        }
+    }
+}
